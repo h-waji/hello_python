030e0c5564bbc3b3d5a7c801e9c6309ff125c51f
wajima <goo.hiro.w@gmail.com>
2023-07-30 21:37:31 +0900

    tuple

---
+++ 04_tuple.py
@@ -0,0 +1,24 @@
+num_touple = (10, 20)
+print(num_touple)
+
+x, y = num_touple
+print(x, y)
+
+x, y = 10, 20
+print(x, y)
+
+min, max = 0, 777
+print(min, max)
+
+i = 11
+j = 22
+tmp = i
+i = j
+j = tmp
+print(i, j)
+
+a = 111
+b = 222
+print(a, b)
+a, b = b, a
+print(a, b)
